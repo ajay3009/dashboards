d0b5c67da6abe03f8275127da67a88c55cb40c00
ajay kumar <ajaysandy@gmail.com>
2018-02-12 04:31:27 +0530

    Added routing

--- inkjectdashboard/src/app/app.module.ts
+++ inkjectdashboard/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MainDashboardComponent } from './main-dashboard/main-dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'login' , component: LoginComponent },
+  { path: 'dashboard' , component: MainDashboardComponent },
+  { path:'', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainDashboardComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
